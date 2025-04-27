2bbff640f1868d51b7e9ad67e532642a4a2dcea3
uluagac <ersinuluagac@gmail.com>
2025-04-27 22:16:15 +0300

    Edited comment lines.

--- Service/Interfaces/ICategoryService.cs
+++ Service/Interfaces/ICategoryService.cs
@@ -4,6 +4,11 @@ namespace Service.Interfaces
 {
   public interface ICategoryService
   {
+    /// <summary>
+    /// Tüm kategorileri (Category) veri tabanından getirir.
+    /// </summary>
+    /// <param name="trackChanges">Değişiklik takibi yapılacak mı?</param>
+    /// <returns>Kategorilerin listesi.</returns>
     IEnumerable<Category> GetAllCategories(bool trackChanges);
   }
 }
--- Service/Interfaces/IPostService.cs
+++ Service/Interfaces/IPostService.cs
@@ -5,11 +5,46 @@ namespace Service.Interfaces
 {
   public interface IPostService
   {
+    /// <summary>
+    /// Tüm gönderileri (Post'lar) veri tabanından getirir.
+    /// </summary>
+    /// <param name="trackChanges">Değişiklik takibi yapılacak mı?</param>
+    /// <returns>Gönderiler listesi.</returns>
     IEnumerable<Post> GetAllPosts(bool trackChanges);
+
+    /// <summary>
+    /// Verilen ID'ye sahip tek bir gönderiyi (Post) getirir.
+    /// </summary>
+    /// <param name="id">Getirilecek gönderinin benzersiz kimliği (ID).</param>
+    /// <param name="trackChanges">Değişiklik takibi yapılacak mı?</param>
+    /// <returns>Verilen ID'ye sahip gönderi (Post) ya da eğer bulunamazsa null döner.</returns>
     Post? GetOnePost(int id, bool trackChanges);
+
+    /// <summary>
+    /// Verilen ID'ye sahip tek bir gönderiyi (Post) güncelleme için alır.
+    /// </summary>
+    /// <param name="id">Güncellenmek istenen gönderinin benzersiz kimliği (ID).</param>
+    /// <param name="trackChanges">Değişiklik takibi yapılacak mı?</param>
+    /// <returns>Verilen ID'ye sahip gönderinin güncellenebilir versiyonunu döner (PostDto). 
+    /// Eğer gönderi bulunamazsa, null döner.</returns>
     PostDto GetOnePostForUpdate(int id, bool trackChanges);
+
+    /// <summary>
+    /// Yeni bir gönderi (Post) oluşturur.
+    /// </summary>
+    /// <param name="postDto">Oluşturulacak gönderinin verilerini içeren DTO.</param>
     void CreateOnePost(PostDto postDto);
+
+    /// <summary>
+    /// Mevcut bir gönderiyi günceller.
+    /// </summary>
+    /// <param name="postDto">Güncellenmesi gereken gönderinin verilerini içeren DTO.</param>
     void UpdateOnePost(PostDto postDto);
+    
+    /// <summary>
+    /// Belirli bir gönderiyi siler.
+    /// </summary>
+    /// <param name="id">Silinecek gönderinin benzersiz kimliği.</param>
     void DeleteOnePost(int id);
   }
 }
